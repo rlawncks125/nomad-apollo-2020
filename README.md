2d8142ddd2df8100894be7fa1fdc9bfc339585c2
rlawncks125 <rlawncks125@naver.com>
2021-01-26 15:52:01 +0900

    ApolloClient End

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Apollo 2020
 
 Movie app built with React , Apollo and GraphQL
+
+데이터 : http://yts.lt/api/v2/list_movies.json
---
+++ src/routes/Home.js
@@ -0,0 +1,94 @@
+import React from "react";
+import Movie from "../components/Movie";
+import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+// import { renderIntoDocument } from "react-dom/test-utils";
+
+const GET_MOVIES = gql`
+  {
+    movies(limit: 25) {
+      id
+      title
+      rating
+      description_intro
+      language
+      medium_cover_image
+      isLiked @client
+      genres
+    }
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
+const Home = () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2020</Title>
+        <Subtitle>I love GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      <Movies>
+        {data?.movies?.map((m) => {
+          if (m.language === "") return "";
+
+          return (
+            <Movie
+              key={m.id}
+              id={m.id}
+              isLiked={m.isLiked}
+              bg={m.medium_cover_image}
+            />
+          );
+        })}
+      </Movies>
+    </Container>
+  );
+};
+
+export default Home;
